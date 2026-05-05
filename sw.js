679c8719d3c35fa82989133e2fad760c8b450d33
pedromosquedapedro-gif <pedromosquedapedro@gmail.com>
2026-05-05 16:08:25 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,19 @@
-const CACHE_NAME = 'vextel-v1';
-
+const CACHE_NAME = 'vextel-v5';
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(['/vextel/', '/vextel/index.html']);
     })
   );
 });
-
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
+});
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => {
